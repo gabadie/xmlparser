ac6b79065540eafaff33017676be10451dcf4504
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-14 22:24:50 +0100

    Setups Flex and Bison compilation

--- candy_project.mk
+++ candy_project.mk
@@ -4,12 +4,41 @@
 # ------------------------------------------------------------ default configuration
 override config=debug
 
-PROJECT_CXXFLAGS := -Wall -Wextra -g -DAPP_DEBUG
+PROJECT_CXXFLAGS := -Wall -Wextra -g -DAPP_DEBUG -DYYDEBUG
+PROJECT_BISONFLAGS := --debug
+PROJECT_FLEXFLAGS := --debug
 
 # ------------------------------------------------------------ debug configuration
 $(call trash_configs, debug)
 $(call hook_precommit_configs, debug)
 
+
+# ------------------------------------------------------------------------------ Flex's file
+
+APP_FLEX_FILE = src/LexicalAnalyser.lex
+APP_FLEX_TARGET = $(patsubst %,$(BUILD_SRC_GEN_DIR)%.cpp, $(notdir $(APP_FLEX_FILE)))
+
+$(APP_FLEX_TARGET): $(APP_FLEX_FILE)
+	$(call history_rule,compiling flex file,$<)
+	$(CMD_MKDIR_ALL) $(dir $@)
+	$(CMD_PREFIX)flex -o $@ $(PROJECT_FLEXFLAGS) $<
+
+
+# ------------------------------------------------------------------------------ Bison's file
+
+APP_BISON_FILE = src/GrammarSpecification.y
+APP_BISON_TARGET = $(patsubst %,$(BUILD_SRC_GEN_DIR)%.cpp, $(notdir $(APP_BISON_FILE)))
+
+$(APP_BISON_TARGET): $(APP_BISON_FILE)
+	$(call history_rule,compiling bison file,$<)
+	$(CMD_MKDIR_ALL) $(dir $@)
+	$(CMD_PREFIX)bison -o $@ -d $(PROJECT_BISONFLAGS) $<
+
+
+# ------------------------------------------------------------------------------ Flex and Bison's binaries
+
+APP_FB_BINARIES := $(call bin_object_files,$(APP_FLEX_TARGET) $(APP_BISON_TARGET))
+$(APP_FB_BINARIES): CXXFLAGS = -I src/
 
 # ------------------------------------------------------------------------------ Application's binaries
 
@@ -17,11 +46,12 @@ APP_BINARIES_PRODUCT := $(call product_create,BINLIBSTATIC,application_lib)
 APP_BINARIES_TARGET := $(call product_target,$(APP_BINARIES_PRODUCT))
 $(call product_public,$(APP_BINARIES_PRODUCT))
 
-APP_OBJECT_BINARIES := $(call bin_object_files,$(call filelist,./src/application_lib.flist))
+APP_CPP_FILES := $(call filelist,./src/application_lib.flist)
+APP_OBJECT_BINARIES := $(call bin_object_files,$(APP_CPP_FILES))
 
 # ------------------------------------------------------------ compilation/link configuration
-$(APP_BINARIES_TARGET): $(APP_OBJECT_BINARIES)
-$(APP_BINARIES_TARGET): CXXFLAGS += $(PROJECT_CXXFLAGS)
+$(APP_OBJECT_BINARIES): CXXFLAGS += $(PROJECT_CXXFLAGS)
+$(APP_BINARIES_TARGET): $(APP_OBJECT_BINARIES) $(APP_FB_BINARIES)
 $(APP_BINARIES_TARGET): ARFLAGS += $(APP_OBJECT_BINARIES)
 
 
---
+++ src/LexicalAnalyser.h
@@ -0,0 +1,9 @@
+/* heading.h */
+
+#define YY_NO_UNPUT
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
