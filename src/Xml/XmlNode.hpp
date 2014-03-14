cf21752b07cdf3e24eebe89d77d3f3a93172da86
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-14 23:24:06 +0100

    Implements style guideline on the Xml::Node class

---
+++ src/Xml/XmlNode.hpp
@@ -0,0 +1,65 @@
+#ifndef _H_XML_NODE
+#define _H_XML_NODE
+
+#include <iostream>
+#include <list>
+#include <string>
+
+namespace Xml
+{
+
+    class Element;
+
+    /**
+     * Defines the abstract class of a node for interface purpose
+     */
+    class Node
+    {
+    public:
+
+        /*
+         * Gets all children nodes
+         *
+         * @return A list of all children nodes (obviously...)
+         */
+        virtual
+        std::list<Node *>
+        childrenNodes() = 0;
+
+        virtual
+        std::list<Node const *> const
+        childrenNodes() const = 0;
+
+        /*
+         * Implements standart stream operator
+         */
+        virtual
+        std::ostream &
+        operator >> (std::ostream & stream) const = 0;
+
+        /*
+         * Destructor
+         */
+        virtual
+        ~Node()
+        {
+
+        }
+
+    };
+
+    /*
+     * Defines a sexier standart stream operator
+     */
+    inline
+    std::ostream &
+    operator << (std::ostream & stream, Node const & node)
+    {
+        node >> stream;
+        return stream;
+    }
+
+}
+
+
+#endif //_H_XML_NODE
