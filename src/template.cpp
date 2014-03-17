de09f9953babaeb85f1cb0f3556eedfe2f939b18
golgoth <adrien.duffy@gmail.com>
2014-03-17 07:31:17 +0100

    added .cpp template for documentation

---
+++ src/template.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file main.c
+ * \brief Programme de tests. 
+ * \author Franck.H
+ * \version 0.1
+ * \date 11 septembre 2007
+ *
+ * Programme de test pour l'objet de gestion des chaînes de caractères Str_t.
+ *
+ */
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * \brief       Calcule la distance entre deux points
+ * \details    La distance entre les \a point1 et \a point2 est calculée par l'intermédiaire
+ *                  des coordonnées des points. (cf #Point)
+ * \param[in]    point1         Point 1 pour le calcul de distance.
+ * \param[out]    point2         Point 2 pour le calcul de distance.
+ * \return    Un \e float représentant la distance calculée.
+ */
+ float maFonction(Point point1, Point point2){
+    float a = 2;
+
+    return a
+ }
