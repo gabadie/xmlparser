c1acf5e1571abef0fd54610c71d8fae70166a700
Guillaume Abadie <guillaume.abadie@gmail.com>
2014-03-20 20:20:34 +0100

    Adds integration tests cmd_{notenough,toomuch}_params

--- tests_integration/test_invocation.sh
+++ tests_integration/test_invocation.sh
@@ -16,21 +16,21 @@ TEST_TMP_STDERR_PATH="$2`basename $3`.stderr.log"
 
 if [ $? -ne $TEST_RETURNCODE ]
 then
-    echo "$2: returned $? instead of $TEST_RETURNCODE";
+    echo "$3: returned $? instead of $TEST_RETURNCODE";
     TEST_STATUS=1;
 fi;
 
 diff -wB $TEST_TMP_STDOUT_PATH $TEST_STDOUT_PATH > /dev/null
 if [ $? -ne 0 ]
 then
-    echo "$2: stdout doesn't match";
+    echo "$3: stdout doesn't match";
     TEST_STATUS=1;
 fi;
 
 diff -wB $TEST_TMP_STDERR_PATH $TEST_STDERR_PATH > /dev/null
 if [ $? -ne 0 ]
 then
-    echo "$2: stderr doesn't match";
+    echo "$3: stderr doesn't match";
     TEST_STATUS=1;
 fi;
 
