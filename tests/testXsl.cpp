3175aa544191ad87f41c24958bfc9f0013f49d1d
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-03-24 14:53:07 +0100

    Started implementing Xsl, added (empty) tests

---
+++ tests/testXsl.cpp
@@ -0,0 +1,13 @@
+#include <mk_test.h>
+
+#include "../src/Xml/XmlParser.hpp"
+#include "../src/Xsl/Xsl.hpp"
+
+#define xml_code(code) \
+    ((const char *) #code)
+
+int
+main()
+{
+    return 0;
+}
