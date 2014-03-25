2978f9edb6fb65317e27c3bcb64a0f8345432ab6
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-03-25 08:29:35 +0100

    Added test structure

--- tests/testXsl.cpp
+++ tests/testXsl.cpp
@@ -5,6 +5,40 @@
 
 #define xml_code(code) \
     ((const char *) #code)
+void
+testXslTransform()
+{
+    // xml
+    std::string xmlContent (xml_code(
+        <root>
+            <cd>
+                <title>Title A</title>
+            </cd>
+
+            <cd>
+                <title>Title B</title>
+            </cd>
+        </root>
+    ));
+
+    Xml::Log xmlLog;
+
+    Xml::Document * xmlDoc = Xml::parse(xmlContent, &xmlLog);
+
+    // xsl
+    std::string xslContent (xml_code(
+        <xsl:stylesheet>
+            <xsl:template match="cd">
+                <xsl:value-of select="title" />
+            </xsl:template>
+        </xsl:stylesheet>
+    ));
+    Xml::Log xslLog;
+    Xml::Document * xslDoc = Xml::parse(xslContent, &xslLog);
+
+    delete xmlDoc;
+    delete xslDoc;
+}
 
 int
 main()
