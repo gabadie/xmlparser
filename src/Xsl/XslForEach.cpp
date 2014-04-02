daf865b43f83f3439cd7f0393605512d4ed4d9b8
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-02 10:04:26 +0200

    Added app_assert

--- src/Xsl/XslForEach.cpp
+++ src/Xsl/XslForEach.cpp
@@ -6,17 +6,25 @@
 #include "../Xml/XmlElement.hpp"
 #include "../Xml/XmlNode.hpp"
 
+#include "../AppDebug.hpp"
+
 
 std::vector<Xml::Node *>
 Xsl::ForEach::operator () (Xml::Element const * context,
     Xml::Document const & xslDoc, Xml::Element const * forEachElement) const
 {
+    app_assert(context != nullptr);
+    app_assert(forEachElement != nullptr);
+
     std::vector<Xml::Node *> resultNodes;
     auto matchingNodes = context->select(forEachElement->attribute("select"));
     for (auto node : matchingNodes)
     {
+        app_assert(node != nullptr);
+
         for (auto resultNode : Xsl::applyTemplate(node, xslDoc, forEachElement))
         {
+            app_assert(resultNode != nullptr);
             resultNodes.push_back(resultNode);
         }
     }
