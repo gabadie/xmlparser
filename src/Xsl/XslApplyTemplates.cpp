983c9e379f902d24732b9d8ef78e7d41ffa9652b
Thierry CANTENOT <thierry.cantenot@insa-lyon.fr>
2014-04-02 10:03:03 +0200

    Added app_assert

--- src/Xsl/XslApplyTemplates.cpp
+++ src/Xsl/XslApplyTemplates.cpp
@@ -1,22 +1,29 @@
-
 #include "XslApplyTemplates.hpp"
 #include "XslTemplate.hpp"
 
 #include "../Xml/XmlElement.hpp"
 #include "../Xml/XmlNode.hpp"
 
+#include "../AppDebug.hpp"
+
 std::vector<Xml::Node *>
 Xsl::ApplyTemplates::operator () (Xml::Element const * context,
     Xml::Document const & xslDoc, Xml::Element const * applyTemplatesElement) const
 {
+    app_assert(context != nullptr);
+    app_assert(applyTemplatesElement != nullptr);
+
     std::vector <Xml::Node*> resultNodes;
 
     if (applyTemplatesElement->attribute("select") == "")
     {
         for (auto element : context->elements())
         {
+            app_assert(element != nullptr);
+
             for (auto appliedElement : findAndApplyTemplate(element, xslDoc))
             {
+                app_assert(appliedElement != nullptr);
                 resultNodes.push_back(appliedElement);
             }
         }
@@ -25,8 +32,11 @@ Xsl::ApplyTemplates::operator () (Xml::Element const * context,
     {
         for (auto element : context->select(applyTemplatesElement->attribute("select")))
         {
+            app_assert(element != nullptr);
+
             for (auto appliedElement : findAndApplyTemplate(element, xslDoc))
             {
+                app_assert(appliedElement != nullptr);
                 resultNodes.push_back(appliedElement);
             }
         }
