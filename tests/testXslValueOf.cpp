8da991bd383fbbc5b37be8ae072cc6eea0b74a9b
golgoth <adrien.duffy@gmail.com>
2014-04-02 22:45:37 +0200

    doc instruction on latex and some more test on value-of

--- tests/testXslValueOf.cpp
+++ tests/testXslValueOf.cpp
@@ -52,7 +52,7 @@ textXslHello()
 {
     std::string xml = xml_code(
         <root>
-            <text> hello <text/>
+            <text> hello </text>
         </root>
     );
 
@@ -116,18 +116,25 @@ textXslNonExist()
 }
 
 void
-textXslNonRecursive()
+textXslRecursive()
 {
     std::string xml = xml_code(
         <root>
             <child>
-                <text> hello <text/>
+                First Level
+                <coui> Second level </coui>
+                <text> Second level Bis </text>
+            </child>
+            <child>
+                First Level
+                <coui> Seconssd level </coui>
+                <text> Second level Bis </text>
             </child>
         </root>
     );
 
     std::string xsl = xml_code(
-        <xsl:value-of select="/" />
+        <xsl:value-of select="/child" />
     );
 
     Xml::Log log;
@@ -143,13 +150,70 @@ textXslNonRecursive()
 
     Xml::Log transformLog;
     auto r = valueOf(xmlDoc->root(), *xslDoc, xslDoc->root(), transformLog);
-    //std::cerr << ((Xml::Element *)r[0])->contentText() << std::endl;
 
-    test_assert(((Xml::Element *)r[0])->contentText() == "hello");
+    // std::cerr << ((Xml::Element *)r[0])->contentText() << std::endl;
+
+    test_assert(((Xml::Element *)r[0])->contentText() == "First LevelSecond levelSecond level Bis");
 
 
     delete xmlDoc;
     delete xslDoc;
+}
+
+
+void
+textXslAttributes()
+{
+    std::string xml = xml_code(
+        <root>
+            <child param1="First">
+                First Level
+                <coui param2="Second"> Second level </coui>
+                <text param3="Third"> Second level Bis </text>
+            </child>
+        </root>
+    );
+
+    std::string xsl1 = xml_code(
+        <xsl:value-of select="child/@param1" />
+    );
+
+    std::string xsl2 = xml_code(
+        <xsl:value-of select="/child/coui/@param2" />
+    );
+
+    std::string xsl3 = xml_code(
+        <xsl:value-of select="child/text/@param3" />
+    );
+
+    Xml::Log log;
+    auto xmlDoc = Xml::parse(xml);
+    auto xslDoc1 = Xml::parse(xsl1);
+    auto xslDoc2 = Xml::parse(xsl2);
+    auto xslDoc3 = Xml::parse(xsl3);
+
+    test_assert(xmlDoc != nullptr);
+    test_assert(xmlDoc != nullptr);
+    test_assert(xslDoc1 != nullptr);
+    test_assert(xslDoc2 != nullptr);
+    test_assert(xslDoc3 != nullptr);
+
+    Xsl::ValueOf valueOf;
+
+    Xml::Log transformLog;
+    auto r1 = valueOf(xmlDoc->root(), *xslDoc1, xslDoc1->root(), transformLog);
+    auto r2 = valueOf(xmlDoc->root(), *xslDoc2, xslDoc2->root(), transformLog);
+    auto r3 = valueOf(xmlDoc->root(), *xslDoc3, xslDoc3->root(), transformLog);
+
+    test_assert(((Xml::Element *)r1[0])->contentText() == "First");
+    test_assert(((Xml::Element *)r2[0])->contentText() == "Second");
+    test_assert(((Xml::Element *)r3[0])->contentText() == "Third");
+
+
+    delete xmlDoc;
+    delete xslDoc1;
+    delete xslDoc2;
+    delete xslDoc3;
 }
 
 
@@ -159,7 +223,8 @@ main()
     testXslEmptyValue();
     textXslHello();
     textXslNonExist();
-    //textXslNonRecursive();
+    textXslRecursive();
+    textXslAttributes();
 
     return 0;
 }
